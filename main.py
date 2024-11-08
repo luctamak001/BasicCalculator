8923c0e0ccd59d8367ac2288e81e55d5b994ef19
Makenley Luctamar <luctamak001@warren.k12.in.us>
2024-11-08 08:25:25 -0500

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+def calculator(num1, num2, operator):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        return num1 / num2
+
+
+# Test cases
+print(calculator(10, 5, '+'))  # Output: 15
+print(calculator(10, 5, '-'))  # Output: 5
+print(calculator(10, 5, '*'))  # Output: 50
+print(calculator(10, 5, '/'))  # Output: 2.0
